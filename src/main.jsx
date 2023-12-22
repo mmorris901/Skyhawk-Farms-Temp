da9ccac786d9b55bf4a50c3e15067de1df4ce4aa
Matthew Morris <matthew.morris@memphistr.org>
2023-12-22 16:49:17 -0600

    Added TODO and changed error page message

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,19 @@ import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import tempControl from "./routes/settings.jsx"
 
+
+
+/* TODO: 
+  1) Establish the errors in the path for router
+  2) Create route for House 1 and House 2 
+  3) Finish UI for H1 and H2
+  4) Implement the tele API
+  5) Update the whole program into a new repository that uses the basic setup and not starter setup from react
+*/ 
+
+
+
+
 const router = createBrowserRouter([
   {
     path: "/",
